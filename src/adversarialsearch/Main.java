343cb34adbbc39650b4f1b551429435a72dad996
Max Lytovka <litmax515@gmail.com>
2023-10-09 15:24:16 +0200

    3 - DONE

--- src/adversarialsearch/Main.java
+++ src/adversarialsearch/Main.java
@@ -2,7 +2,9 @@ package adversarialsearch;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		State state = new State(new int[]{2, 2}, new int[]{3, 2}, new int[]{}, 0, 2);
+		state.read("/Users/admin/Desktop/Jaba/ass1/data/board.txt");
+		System.out.print(state.toString());
 		//Game g=new Game();
 		//g.test();
 	}
---
+++ src/adversarialsearch/State.java
@@ -0,0 +1,53 @@
+package adversarialsearch;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+
+public class State {
+	public char[][] board ; // the board as a 2D character array
+	public int[] agentX ; // the x−coordinates of the agents
+	public int[] agentY ; // the y−coordinates of the agents
+	public int[] score ; // the amount of food eaten by each agent
+	public int turn ; // who’s turn it is, agent 0 or agent 1
+	public int food ; // the total amount of food still available
+	public int nRows;
+	public int nCols;
+	
+	public State(int[] agentX, int[] agentY, int[] score, int turn, int food) {
+		this.agentX = agentX;
+		this.agentY = agentY;
+		this.score = score;
+		this.turn = turn;
+		this.food = food;
+	}
+	
+	public void read(String file) {
+		try {
+			RandomAccessFile boardFile = new RandomAccessFile(file, "r");
+			String line = boardFile.readLine();
+			String[] dimensions = line.split(" ", 2);
+			this.nRows = Integer.parseInt(dimensions[0]);
+			this.nCols = Integer.parseInt(dimensions[1]);
+			this.board = new char[nRows][nCols];
+			for (int i = 0; i < nRows; i++) {
+				line = boardFile.readLine();
+				for (int j = 0; j < nCols; j++) {
+					this.board[i][j] = line.charAt(j);
+				}
+			}
+			boardFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String toString() {
+		String output = "";
+		for (int i = 0; i < nRows; i++) {
+			String row = new String(this.board[i]);
+			output += row + "\n";
+		}
+		return output;
+	}
+}
