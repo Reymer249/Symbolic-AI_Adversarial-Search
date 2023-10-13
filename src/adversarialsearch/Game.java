d72126b59adc8089c83d1be98c1856ffdcf4b99a
IvanBanny <falco.zebrine@gmail.com>
2023-10-13 10:54:32 +0200

    feat(#11, #12): implemented alphabeta

--- src/adversarialsearch/Game.java
+++ src/adversarialsearch/Game.java
@@ -58,16 +58,86 @@ public class Game {
 		}
 	}
 	
+	public State alphabeta(State s, int forAgent, int maxDepth, int depth, double alpha, double beta) {
+		if (depth == maxDepth || s.isLeaf()) {
+			if (s.food == 0 && s.score[0] == 2 && s.score[1] == 0)
+				System.out.println("0win" + s.value(forAgent));
+			else if (s.food == 0 && s.score[1] == 2 && s.score[0] == 0)
+				System.out.println("1win" + s.value(forAgent));
+			return s;
+		}
+		
+		if (s.turn == forAgent ) {
+			double maxEval = -2;
+			Vector<String> legalMoves =  s.legalMoves(s.turn);
+			int n_children = legalMoves.size();
+			State best_state = new State();
+			State child_state;
+			String action;
+			for (int i = 0; i < n_children; i++) {
+				action = legalMoves.get(i);
+				child_state = s.copy();
+				child_state.execute(action);
+				child_state = alphabeta(child_state, forAgent, maxDepth, depth+1, alpha, beta);
+				double value = child_state.value(forAgent);
+				if (value > maxEval) {
+					maxEval = value;
+					best_state = child_state;
+				}
+				if (alpha < value)
+					alpha = value;
+				if (beta <= alpha)
+					break;
+			}
+			return best_state;
+		}
+		else {
+			double minEval = 2;
+			Vector<String> legalMoves =  s.legalMoves(s.turn);
+			int n_children = legalMoves.size();
+			State worst_state = new State(); // We name the variable "worst case" as we look 
+			//from the perspective of <forAgent> agent
+			State child_state;
+			String action;
+			for (int i = 0; i < n_children; i++) {
+				action = legalMoves.get(i);
+				child_state = s.copy();
+				child_state.execute(action);
+				child_state = alphabeta(child_state, forAgent, maxDepth, depth+1, alpha, beta);
+				double value = child_state.value(forAgent);
+				if (value < minEval) {
+					minEval = value;
+					worst_state = child_state;
+				}
+				if (beta > value)
+					beta = value;
+				if (beta <= alpha)
+					break;
+			}
+			return worst_state;
+		}
+	}
+	
 	public void test() {
 //		System.out.println("\nGame.test not fully implemented yet\n");
 		
-		State out = minimax(b, b.turn, 6, 0);
-		System.out.println(out);
-		System.out.println(out.agentX[0] + " " + out.agentY[0]);
-		System.out.println(out.agentX[1] + " " + out.agentY[1]);
-		System.out.println(out.moves);
-		System.out.println(out.score[0] + " " + out.score[1]);
+		State out1 = alphabeta(b, b.turn, 15, 0, -2, 2);
+		System.out.println(out1);
+		System.out.println(out1.value(0));
+		System.out.println(out1.agentX[0] + " " + out1.agentY[0]);
+		System.out.println(out1.agentX[1] + " " + out1.agentY[1]);
+		System.out.println(out1.moves);
+		System.out.println(out1.score[0] + " " + out1.score[1]);
+		System.out.println(out1.food);
 		
+//		System.out.print("---------\n");
+//		
+//		State out2 = minimax(b, b.turn, 10, 0);
+//		System.out.println(out2);
+//		System.out.println(out2.agentX[0] + " " + out2.agentY[0]);
+//		System.out.println(out2.agentX[1] + " " + out2.agentY[1]);
+//		System.out.println(out2.moves);
+//		System.out.println(out2.score[0] + " " + out2.score[1]);
 		
 //		while (!b.isLeaf()){
 //			System.out.println(b.toString());
